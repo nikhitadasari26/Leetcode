6e5ae18fc63939dd1a77096a8ee85a46ceebf475
nikhitadasari26 <147791798+nikhitadasari26@users.noreply.github.com>
2025-09-22 21:36:54 +0530

    Time: 4 ms (17.99%), Space: 42.2 MB (18.22%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(!st.isEmpty() && ((st.peek()=='(' && c==')') || (st.peek()=='[' && c==']') || (st.peek()=='{' && c=='}') ))
+            {
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        
+        return st.isEmpty();
+    }
+}
