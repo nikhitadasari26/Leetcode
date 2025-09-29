38887524eca757af064b05700df9172f1a9d47fc
nikhitadasari26 <147791798+nikhitadasari26@users.noreply.github.com>
2025-09-29 22:08:20 +0530

    Time: 357 ms (5.15%), Space: 46.2 MB (48.2%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer> fre = new HashMap<>();
-        for(int i=0;i<n;i++){
-            fre.put(nums[i],fre.getOrDefault(nums[i],0)+1);
+        for(int i=0;i<nums.length;i++){
+        int c=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]==nums[i]) c++;
         }
-        int s=0;
-        for(int i=0;i<n;i++){
-           if(fre.get(nums[i])==1){
-                s=nums[i];
-                break;
-           }
-        }
-        return s;
+        int k = nums[i];
+        if(c==1) return k;
+    }
+    return -1;
     }
 }
